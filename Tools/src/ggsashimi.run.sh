fbe728e97f6359dbdc47ed2cac7348110688a502
ledak <62183935+LedaKatopodi@users.noreply.github.com>
2022-05-05 16:41:34 -0400

    Add files via upload

--- Tools/src/ggsashimi.run.sh
+++ Tools/src/ggsashimi.run.sh
@@ -4,13 +4,22 @@
 export GGSASHIMI_DEBUG=yes 
 
 ## Get Arguments
-while getopts d:g: flag
+while getopts d:g:c:o: flag
 do
 	case "${flag}" in
 		d) wkDir=${OPTARG};;
 		g) gtf=${OPTARG};;
+		c) coord=${OPTARG};;
+		o) outprefix=${OPTARG};;
 	esac
 done
+
+if [ -z "$coord" ]; then echo "Error: -c argument required."; exit; fi
+if [ -z "$outprefix" ]; then 
+	coord2=$(sed 's/:/-/g' <<< "$coord")
+	outprefix=$(echo $coord2"_Sashimi")
+	echo "Setting output prefix to: " $outprefix
+fi
 
 ## Create Output Directory
 mkdir $wkDir/SashimiPlots
@@ -19,8 +28,8 @@ cd $wkDir/SashimiPlots
 ## Create palette, assuming 2 Groups
 echo -e "#3399FF\n#FF661A" > palette.tsv
 ## Running ggsashimi
-~/ggsashimi.py -b $wkDir/ggsashimi_inputBams.tsv -c 12:6693791-6700815  \
--g $gtf --out-prefix "PIANP_12-6693791-6700815_Sashimi" \
+~/ggsashimi.py -b $wkDir/ggsashimi_inputBams.tsv -c $coord  \
+-g $gtf --out-prefix $outprefix \
 -M 10 -C 3 -O 3 --alpha 0.25 --base-size 20 --ann-height 1.5 --height 3 --width 10 --shrink \
 --palette palette.tsv --fix-y-scale --out-format png
 ## Generatinf Plot
